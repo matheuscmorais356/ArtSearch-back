fba4a21bfd4713114d62a1fd140e162e53d6218a
matheuscmorais356 <matheuscmorais356@gmail.com>
2024-04-30 13:58:33 -0300

    Adicionando a rota /

--- routes/Router.js
+++ routes/Router.js
@@ -4,9 +4,13 @@ const router = express();
 
 const { searchArtist, genarateRandomArtist, albumTracks } = require("../controllers/ArtistController");
 
+router.get("/", (req, res) => {
+  res.send("Hello World")
+});
+
 router.get("/api/v1/ping", (req, res) => {
   res.send("pong")
-})
+});
 
 router.post("/api/v1/checkmusic", searchArtist);
 router.post("/api/v1/randomArtist", genarateRandomArtist);
